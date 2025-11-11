bff227719f00f26b0d7aace94e5158089dcf4374
BenJako1 <132501374+BenJako1@users.noreply.github.com>
2025-11-10 21:11:05 -0600

    Convection BC

--- 1D.py
+++ 1D.py
@@ -21,16 +21,21 @@ class Bar:
             self.elements[i, 0] = i
             self.elements[i, 1] = i+1
 
+        self.element_len = np.zeros([N-1, 1])
+        for i in range(self.N-1):
+            self.element_len[i] = ((self.x[i+1] - self.x[i]))
+
         if verbose:
             print(f'Nodes at: x={self.x}')
             print(f'Elements with nodes: \n {self.elements}')
+            print(f'Elements lengths: \n {self.element_len}')
 
     def assemble_conductance(self, verbose=False):
         element_conductance = np.array([[1, -1],
                                        [-1, 1]])
         self.Conductance = np.zeros([self.N, self.N])
         for i in range(self.N-1):
-            coefficient = self.k * self.A / (self.x[i+1] - self.x[i])
+            coefficient = self.k * self.A / self.element_len[i]
             self.Conductance[i, i] += element_conductance[0, 0] * coefficient
             self.Conductance[i, i+1] += element_conductance[0, 1] * coefficient
             self.Conductance[i+1, i] += element_conductance[1, 0] * coefficient
@@ -53,20 +58,20 @@ class Bar:
                 node_range = range(*parts)
                 nodes_unpacked.extend(node_range)
                 types_unpacked.extend([type] * len(node_range))
-                values_unpacked.extend([value] * len(node_range))
+                values_unpacked.extend([float(value)] * len(node_range))
             elif isinstance(node, (list, tuple)):
                 nodes_unpacked.extend(node)
                 types_unpacked.extend([type] * len(node))
-                values_unpacked.extend([value] * len(node))
+                values_unpacked.extend([float(value)] * len(node))
             else:
                 nodes_unpacked.append(int(node))
                 types_unpacked.append(type)
-                values_unpacked.append(value)
+                values_unpacked.append(float(value))
 
-        self.T = np.full(shape=[self.N], fill_value=None, dtype=object)
+        self.T = np.full(shape=[self.N], fill_value=0, dtype=object)
         self.Q = np.full(shape=[self.N], fill_value=0, dtype=object)
+        self.Convection = np.full(shape=[self.N], fill_value=0, dtype=object)
         self.boundNodes = []
-
         for i in range(len(nodes_unpacked)):
             if types_unpacked[i] == "temp":
                 self.T[nodes_unpacked[i]] = values_unpacked[i]
@@ -74,30 +79,52 @@ class Bar:
                     self.Q[nodes_unpacked[i]] = None
                 self.boundNodes.append(nodes_unpacked[i])
             elif types_unpacked[i] == "flux":
-                self.Q[nodes_unpacked[i]] += values_unpacked[i]
+                if self.Q[nodes_unpacked[i]+1] == None:
+                    self.Q[nodes_unpacked[i]] += values_unpacked[i]
+                else:
+                    self.Q[nodes_unpacked[i]] = values_unpacked[i]
             elif types_unpacked[i] == "gen":
                 if self.Q[nodes_unpacked[i]] == None:
-                    self.Q[nodes_unpacked[i]] = self.A * values_unpacked[i] * \
-                                                    (self.x[nodes_unpacked[i]+1] - self.x[nodes_unpacked[i]]) / 2
+                    self.Q[nodes_unpacked[i]] = self.A * values_unpacked[i] * self.element_len[nodes_unpacked[i]] / 2
                 else:
-                    self.Q[nodes_unpacked[i]] += self.A * values_unpacked[i] * \
-                                                    (self.x[nodes_unpacked[i]+1] - self.x[nodes_unpacked[i]]) / 2
+                    self.Q[nodes_unpacked[i]] += self.A * values_unpacked[i] * self.element_len[nodes_unpacked[i]] / 2
                 if self.Q[nodes_unpacked[i]+1] == None:
-                    self.Q[nodes_unpacked[i]+1] = self.A * values_unpacked[i] * \
-                                                    (self.x[nodes_unpacked[i]+1] - self.x[nodes_unpacked[i]]) / 2
+                    self.Q[nodes_unpacked[i]+1] = self.A * values_unpacked[i] * self.element_len[nodes_unpacked[i]] / 2
                 else:
-                    self.Q[nodes_unpacked[i]+1] += self.A * values_unpacked[i] * \
-                                                (self.x[nodes_unpacked[i]+1] - self.x[nodes_unpacked[i]]) / 2
+                    self.Q[nodes_unpacked[i]+1] += self.A * values_unpacked[i] * self.element_len[nodes_unpacked[i]] / 2
+            elif types_unpacked[i] == "convFace":   # Requires h*T_inf as input
+                if self.Q[nodes_unpacked[i]] == None:
+                    self.Convection[nodes_unpacked[i]] = float(values_unpacked[i]) * self.A
+                    self.Q[nodes_unpacked[i]] += float(values_unpacked[i]) * self.A
+                else:
+                    self.Convection[nodes_unpacked[i]] += float(values_unpacked[i]) * self.A
+                    self.Q[nodes_unpacked[i]] = float(values_unpacked[i]) * self.A
+            elif types_unpacked[i] == "convSurf":   # Requires h*T_inf*A_s as input
+                if self.Q[nodes_unpacked[i]] == None:
+                    self.Convection[nodes_unpacked[i]] += float(values_unpacked[i]) * self.A * self.element_len[nodes_unpacked[i]]
+                    self.Q[nodes_unpacked[i]] = float(values_unpacked[i]) * self.A * self.element_len[nodes_unpacked[i]]
+                else:
+                    self.Convection[nodes_unpacked[i]] += float(values_unpacked[i]) * self.A * self.element_len[nodes_unpacked[i]]
+                    self.Q[nodes_unpacked[i]] += float(values_unpacked[i]) * self.A * self.element_len[nodes_unpacked[i]]
+            
                 
         self.freeNodes = [int(i) for i in range(self.N) if i not in self.boundNodes]
 
         if verbose:
             print(f'Temperature vector: {self.T}')
             print(f'Flux vector: {self.Q}')
+            print(f'Convection vector: {self.Convection}')
 
     def solve(self):
         solutionArray = self.Conductance
         Q = self.Q
+
+        if any(self.Convection) != 0:
+            for i in range(len(self.Convection)):
+                solutionArray[i,i] += self.Convection[i]
+        
+        print(self.Convection)
+        print(solutionArray)
 
         for i in range(len(self.boundNodes)):
             solutionArray = np.delete(solutionArray, self.boundNodes[i] - i, axis=0)
@@ -125,21 +152,26 @@ class Bar:
         return T_sol, Q_sol
 
 if __name__ == "__main__":
-    sim = Bar(k=25, A=1)
-    sim.geometry(L=1, N=5, verbose=False)
+    sim = Bar(k=0.1, A=1)
+    sim.geometry(L=1, N=20, verbose=False)
     sim.assemble_conductance(verbose=False)
-    boundDict = {"nodes": [0, 4], "type": ["flux", "temp"], "value": [400,0]}
-    sim.bound(boundDict, verbose=False)
+    propertiesDict = {"nodes": [2]}
+    boundDict = {"nodes": [0,"0:19"], "type": ["temp","convSurf"], "value": [80,10]}
+    sim.bound(boundDict, verbose=True)
     T, Q = sim.solve()
 
-    print(T, Q)
+    print(T)
 
     plt.plot(sim.x, T)
+    plt.xlim(0,max(sim.x))
     plt.grid()
     plt.show()
 
 """
 TO DO:
-- Add convection BC
+- Add "preperties" dict to allow for non-uniform h and k and T_inf
 - Edge case check especially on back calculating Q
+- Allow for separate h, T_inf and A_s inputs
+- Evaluate whether datatype "None" should realy be used
+- Figure out if the behaviours when convFace input < 0 is legit
 """
